c15b7361fb0645a8659d32c13ccf6c92aa75bb51
Rene Vallecillo <reneval@gmail.com>
2017-03-09 20:32:58 -0600

    adding subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306023424) do
+ActiveRecord::Schema.define(version: 20170310022812) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20170306023424) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
